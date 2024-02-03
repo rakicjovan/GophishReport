2f2ef1de41828b9d54890da9e8607d21be82bdeb
jole583 <115352752+jole583@users.noreply.github.com>
2024-02-04 00:14:40 +0100

    Added checkmark and licensing information

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const ridAlternative = "keyname";
 let headP = document.getElementById("headP");
 let ridRegex = /https:\/\/[A-Za-z]+\.[A-Za-z][A-Za-z]\/\?rid=[A-Za-z0-9]+/i;
 let reportButton = document.getElementById("reportButton");
+let checkmarkHTML = '<svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg>';
+
+
 
 if (ridAlternative) {
     ridRegex = new RegExp('https://[A-Za-z]+\\.[A-Za-z][A-Za-z]/\\?' + ridAlternative + '=[A-Za-z0-9]+', 'i');
@@ -59,11 +62,37 @@ async function webReport(reportUrl) {
         else {
             headP.innerHTML = "Successfully reported the mail, you can delete it now!" + response.status;
             reportButton.disabled = true;
+            document.getElementById("wrapperId").insertAdjacentHTML("beforeend", checkmarkHTML);
+            console.log(document.getElementById("wrapperId").innerHTML);
         }
     }
     catch (error) {
         console.log("test");
         headP.innerHTML = "Error: " + error;
         document.getElementById("reportButton").disabled = true;
+
+        document.getElementById("wrapperId").insertAdjacentHTML("beforeend", checkmarkHTML);
+        console.log(document.getElementById("wrapperId").innerHTML);
     }
+};
+
+function showInfoBox() {
+    let infoModal = document.getElementById("infoModal");
+    infoModal.style.display = "block";
+    setTimeout(function() {
+        infoModal.style.opacity = 1;
+    }, 10);
+};
+
+function closeInfoBox() {
+    document.getElementById("infoModal").style.display = "none";
+};
+
+function openGitHub() {
+    // Redirect to your GitHub page or perform any GitHub-related action
+    window.open("https://github.com/jole583/Goreport", "_blank");
+};
+
+function addCheckmark() {
+
 };
